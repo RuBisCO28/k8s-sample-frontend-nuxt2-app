e513e022e1bfc1f8d4c982889eaf74a9b3adac96
RuBiCO28 <54470624+RuBisCO28@users.noreply.github.com>
2024-01-15 21:41:14 +0900

    add console log

--- pages/price/index.vue
+++ pages/price/index.vue
@@ -2,11 +2,14 @@
   export default {
 		async asyncData({ $axios }) {
 			const url = "/api/hello";
+      console.log("Hello");
 			const response = await $axios.$get(url).then((res) => {
+        console.log("Hello2");
         return res;
       }).catch((err) => {
         console.log(err);
       });
+      console.log("Hello3");
 			return {
 				posts: response
 			};
